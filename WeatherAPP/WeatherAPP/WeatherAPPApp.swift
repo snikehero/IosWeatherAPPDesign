4417ee842706450e757fcf89dccc06112abd1300
snikehero <snikehero234@gmail.com>
2023-06-14 21:16:33 -0600

    Added WeatherAPP swiftui Project

---
+++ WeatherAPP/WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by Moises Sanchez on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
